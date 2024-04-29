f6ba3d85fd4b7d4476ffa234a706e149399faea5
Andrew Lockard <alockard@unc.edu>
2024-04-29 15:01:38 -0400

    Remove redudant backend files

--- Spotify Tinder/src/server/db_setup.mjs
+++
@@ -1,7 +0,0 @@
-import { db } from "./db.mjs";
-
-db.run(`DROP TABLE IF EXISTS Users`);
-
-db.run(`CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY_KEY, userName varchar(50) NOT NULL, password varchar(256) NOT NULL)`);
-
-db.close();
